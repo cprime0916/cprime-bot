ef956b6f94012f205865bcfebc09d21ff963fc53
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-06-05 17:52:45 +0800

    Initialize new branch for contest_cmd implementation

--- src/cmd/contest_cmd.rs
+++ src/cmd/contest_cmd.rs
@@ -1,11 +1,41 @@
-#![allow(dead_code, unreachable_code, unused_variables)]
+#![allow(dead_code, unreachable_code, unused_variables, unused_imports)]
 
+use std::fs;
+use toml;
+use reqwest::{Client, header};
 use poise::Command;
-use crate::{Cmd, Context, Data, Error};
+use http::{Method, StatusCode};
+use http::status::InvalidStatusCode;
+use crate::{Cmd, config, Context, Data, Error, error_print};
 
 pub(crate) struct ContestCmd;
 
-fn get_contests(hosts: Vec<String>) -> !{
+
+async fn get_contests(hosts: Vec<String>) -> !{
+    let toml_info = fs::read_to_string("config.toml").unwrap();
+    let config_toml: config::Config = toml::from_str(&toml_info).unwrap();
+    let mut query_vec = vec![("username", config_toml.clist.username),
+                             ("api_key", config_toml.clist.key)
+                             , ("upcoming", "true".to_owned())];
+    for host in hosts{
+        query_vec.push(("host", host));
+        let response = Client::builder()
+            .build()
+            .unwrap()
+            .get(&config_toml.clist.url)
+            .query(&query_vec)
+            .send()
+            .await?;
+        let data = response.json().await?;
+        match response.status(){
+            StatusCode::OK => {
+                todo!("Implement adding contest info to a container");
+            }
+            _ => {
+                todo!("Settle StatusCode Err");
+            }
+        }
+    }
     todo!("Implement fetching & returning contest data as a Vec.");
 }
 
--- src/config.rs
+++ src/config.rs
@@ -3,9 +3,17 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct Config{
     pub discord: Discord,
+    pub clist: Clist,
 }
 
 #[derive(Deserialize)]
 pub struct Discord{
     pub token: String,
+}
+
+#[derive(Deserialize)]
+pub struct Clist{
+    pub username: String,
+    pub url: String,
+    pub key: String,
 }
